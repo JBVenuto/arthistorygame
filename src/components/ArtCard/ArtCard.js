c0e6352d7270f3c8591a61a02ef9011755963f8f
Joseph Venuto <jbvenuto@gmail.com>
2018-09-14 13:18:32 -0400

    started replacing the local location of the images with the url for the image

--- src/components/ArtCard/ArtCard.js
+++ src/components/ArtCard/ArtCard.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const ArtCard = props => (
     <div className="card">
-        <div className="img-container">
-            <img alt={props.name} src={props.location} />
-        </div>
+        <span className="img-container">
+            <img alt={props.name} src={props.location} width="250px" height="250px"/>
+        </span>
     </div>
 );
 
