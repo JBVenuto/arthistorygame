251f82e0fd2c556434f66465ecaea6d886caaf13
Joseph Venuto <jbvenuto@gmail.com>
2018-09-13 22:07:05 -0400

    filled out app.js. it should be able to render components

---
+++ src/App.js
@@ -0,0 +1,26 @@
+//Files to import
+import React, { Component } from "react";
+import art from "./art.json";
+import ArtCard from "./components/ArtCard"
+
+class App extends Component {
+    state = {
+        art
+    };
+
+    // Go over art.json and rendor the art work on the page
+    render() {
+        return (
+            {this.state.art.map(art => (
+                <ArtCard 
+                    id={art.id}
+                    key={art.id}
+                    name={art.name}
+                    location={art.location}
+                />
+            ))}
+        );
+    }
+}
+
+export default App;
