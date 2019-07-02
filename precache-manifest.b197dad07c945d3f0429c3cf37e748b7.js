b0cf6998a3631a4ea8556f6fa52ca2e78ace0002
Joseph Venuto <jbvenuto@gmail.com>
2019-07-02 13:13:33 -0400

    Updates

---
+++ precache-manifest.b197dad07c945d3f0429c3cf37e748b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99dcb7337e0683c865e1dc0f1f5bdec8",
+    "url": "/arthistorygame/index.html"
+  },
+  {
+    "revision": "e54db0980f0d67985880",
+    "url": "/arthistorygame/static/css/main.04f6ea82.chunk.css"
+  },
+  {
+    "revision": "5e83fa4ab5125e21620e",
+    "url": "/arthistorygame/static/js/2.09f939e4.chunk.js"
+  },
+  {
+    "revision": "e54db0980f0d67985880",
+    "url": "/arthistorygame/static/js/main.1ecfa015.chunk.js"
+  },
+  {
+    "revision": "bc328ea1aae15c6eb127",
+    "url": "/arthistorygame/static/js/runtime~main.e642a80b.js"
+  }
+]);
